06411dee40fa1890510791cf70f90383285dc5ac
Zindesss <zindesss@Truongs-MacBook-Air.local>
2021-12-25 22:12:36 +0700

    push code todo-list with json-server create api, CRD not U

---
+++ src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+    <h3>version 1.0.0</h3>
+    <router-link to="/">Go back</router-link>
+</template>
+<script>
+export default {
+    name: 'About',
+    inheritAttrs: false, // disable 'no-props' warning
+}
+</script>
